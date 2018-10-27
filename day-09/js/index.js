82be66740087a2b20888ef430b8802af59d05553
Stefan(W10) <nertzkoban+GitHub@gmail.com>
2018-10-27 16:04:14 -0700

    in-class work

--- day-09/js/index.js
+++ day-09/js/index.js
@@ -2,10 +2,10 @@
 'use strict';
 
 // Constants / global variables
-const height = 255;
-const width = 255;
-const fill = '#d3d3d3';
-const namespace = 'http://www.w3.org/2000/svg';
+const HEIGHT = 255;
+const WIDTH = 255;
+const FILL = '#d3d3d3';
+const NAMESPACE = 'http://www.w3.org/2000/svg';
 
 // State of our application
 let state = {
@@ -13,67 +13,96 @@ let state = {
 };
 
 // Create a `p` element DOM
-
+let p = document.createElement('p');
 
 // Set the inner HTML of the paragraph to be some bold text
-
+p.innerHTML = '<strong>This is my paragraph</strong>';
 
 // Select the element with class `container`, and append the paragraph to it
-
+document.querySelector('.container').append(p);
 
 // Select `svg` element from the DOM
-
+let svg = document.querySelector('svg');
 
 // Set the width and height attributes using `setAttribute`
-
+svg.setAttribute('height', HEIGHT);
+svg.setAttribute('width', WIDTH);
 
 // Add a `rect` with the same width and height, and set the fill to `fill`
 // Note, you'll have to use `createElementNS` with our namespace
 // Also, set the pointerEvents to 'none'. Then, append this `rect` to the svg
-
+let rect = document.createElementNS(NAMESPACE, 'rect');
+rect.setAttribute('x', 0);
+rect.setAttribute('y', 0);
+rect.setAttribute('height', HEIGHT);
+rect.setAttribute('width', WIDTH);
+rect.setAttribute('fill', FILL);
+rect.style.pointerEvents = 'none';
+svg.appendChild(rect);
 
 // Create a button with class "btn center-align" and append it to the element with class container.
 // You want to have it *inserted before* the div with class `svg-wrapper`
-
+let button = document.createElement('button');
+button.className = "btn center-align";
+button.textContent = "Clear Me!";
+let svgWrapper = document.querySelector(".svg-Wrapper");
+document.querySelector(".container").insertBefore(button, svgWrapper);
 
 // Function to render a circle in a parent element
 function renderCircle(coord, parent) {
     // Create the circle
-
-
+    let circle = document.createElementNS(NAMESPACE, 'circle');
+    circle.setAttribute('cx',coord.x);
+    circle.setAttribute('cy',coord.y);
+    circle.setAttribute('r',10);
     // Define styles
+    circle.style.opacity = 0.3;
+    circle.style.pointerEvents = 'none';
+    circle.style.fill = `rgb(0, ${coord.x}, ${coord.y})`;
 
     // Append
-
+    parent.appendChild(circle);
 }
 
 // Function to draw all circles in a parent
 function drawCircles(data, parent) {
-
+    data.forEach(function(d) {
+        renderCircle(d, parent);
+    })
 }
 
 // Function to clear circles from a parent
 function clearCircles(parent) {
-
+    let elems = parent.querySelectorAll('circle');
+    elems.forEach(function(d) {
+        parent.removeChild(d);
+    })
 }
 
 // Assign event listener - on click:
 // - push a new data element into the state
 // - call the `update` function
-
+svg.addEventListener('click', function(event) {
+    state.circles.push({x: event.offsetX, y:event.offsetY});
+    update();
+});
 
 // Update function: call `clearCircles`, `drawCircles`, and `updateText`
 function update() {
     // Clear container
-
+    let container = svg;
+    clearCircles(container);
     // Update container contents
-
+    drawCircles(state.circles, container);
+    updateText(state.circles);
 }
 
 // Text function: change paragraph text to display the number of circles, and avg. color.
 // Avg. color (in rgb) is the avg. x position (green) and avg. y position (blue)
 // Set the text to be that color
 function updateText(data) {
+
+    let text = document.querySelector('p');
     // Compute average in x direction (green)
 
 
@@ -84,8 +113,12 @@ function updateText(data) {
 
 
     // Set text string and the color of the element
-
+    text.innerHTML = `There are ${data.length} circles on this page.`
 }
 
 // Assign an event listener for the `clear` button
 // Reset state.circles to an empty array and then `update`
+button.addEventListener('click', function() {
+    state.circles = [];
+    update();
+})
